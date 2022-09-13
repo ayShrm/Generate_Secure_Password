fd85cd4da11124bcf1666c8f00c9a25b7f383934
Ayush Sharma <110625033+ayShrm@users.noreply.github.com>
2022-09-13 15:31:52 +0530

    Update generateSecurePassword.java

--- generateSecurePassword.java
+++ generateSecurePassword.java
@@ -1,1 +1,26 @@
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 
+public class generatePassword {
+
+	public static String generateSecurePwd() {
+		CharacterRule LCR = new CharacterRule(EnglishCharacterData.LowerCase);
+		LCR.setNumberOfCharacters(2);
+
+		CharacterRule UCR = new CharacterRule(EnglishCharacterData.UpperCase);
+		UCR.setNumberOfCharacters(2);
+
+		CharacterRule DR = new CharacterRule(EnglishCharacterData.Digit);
+		DR.setNumberOfCharacters(2);
+
+		CharacterRule SR = new CharacterRule(EnglishCharacterData.Special);
+		SR.setNumberOfCharacters(2);
+
+		PasswordGenerator passGen = new PasswordGenerator();
+
+		String password = passGen.generatePassword(8, SR, LCR, UCR, DR);
+
+		return password;
+	}
+}
