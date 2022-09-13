4f9d6c90f1d24d4d1dc6aa32bc47d1d4554adcaa
Ayush Sharma <110625033+ayShrm@users.noreply.github.com>
2022-09-13 16:19:24 +0530

    Update generateSecurePassword.java

--- generateSecurePassword.java
+++ generateSecurePassword.java
@@ -4,23 +4,36 @@ import org.passay.PasswordGenerator;
 
 public class generatePassword {
 
-	public static String generateSecurePwd() {
-		CharacterRule LCR = new CharacterRule(EnglishCharacterData.LowerCase);
-		LCR.setNumberOfCharacters(2);
-
-		CharacterRule UCR = new CharacterRule(EnglishCharacterData.UpperCase);
-		UCR.setNumberOfCharacters(2);
-
-		CharacterRule DR = new CharacterRule(EnglishCharacterData.Digit);
-		DR.setNumberOfCharacters(2);
-
-		CharacterRule SR = new CharacterRule(EnglishCharacterData.Special);
-		SR.setNumberOfCharacters(2);
-
-		PasswordGenerator passGen = new PasswordGenerator();
-
-		String password = passGen.generatePassword(8, SR, LCR, UCR, DR);
-
-		return password;
-	}
+	 // create generateSecurePassword() method that find the secure password and returns it to the main() method  
+    public static String generateSecurePassword() {  
+          
+        // create character rule for lower case  
+        CharacterRule LCR = new CharacterRule(EnglishCharacterData.LowerCase);  
+        // set number of lower case characters  
+        LCR.setNumberOfCharacters(2);  
+  
+        // create character rule for upper case  
+        CharacterRule UCR = new CharacterRule(EnglishCharacterData.UpperCase);  
+        // set number of upper case characters  
+        UCR.setNumberOfCharacters(2);  
+  
+        // create character rule for digit  
+        CharacterRule DR = new CharacterRule(EnglishCharacterData.Digit);  
+        // set number of digits  
+        DR.setNumberOfCharacters(2);  
+  
+        // create character rule for lower case  
+        CharacterRule SR = new CharacterRule(EnglishCharacterData.Special);  
+        // set number of special characters  
+        SR.setNumberOfCharacters(2);  
+          
+        // create instance of the PasswordGenerator class   
+        PasswordGenerator passGen = new PasswordGenerator();  
+          
+        // call generatePassword() method of PasswordGenerator class to get Passay generated password  
+        String password = passGen.generatePassword(8, SR, LCR, UCR, DR);  
+          
+        // return Passay generated password to the main() method   
+        return password;  
+    }  
 }
